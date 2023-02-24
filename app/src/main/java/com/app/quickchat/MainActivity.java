26e40b431e3c965f9cb5f3cfbcc46d8e6c6969ca
takieddine12 <algeriahero22@gmail.com>
2023-02-24 22:10:35 +0100

    new changes

---
+++ app/src/main/java/com/app/quickchat/MainActivity.java
@@ -0,0 +1,61 @@
+package com.app.quickchat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageView;
+
+import java.util.prefs.PreferenceChangeEvent;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    private ImageView refresh;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+         refresh = findViewById(R.id.refresh);
+         webView = findViewById(R.id.webView);
+         loadUrl();
+
+
+
+         refresh.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 loadUrl();
+             }
+         });
+    }
+
+    private void loadUrl(){
+        if(refresh.getVisibility() == View.VISIBLE){
+            refresh.setVisibility(View.GONE);
+        }
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new MyWebViewClient());
+        webView.loadUrl("https://app.quickchat.ai/6q92znlso1/");
+    }
+
+    private  class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView webView, String url) {
+
+            webView.loadUrl(url);
+            return true;
+        }
+
+        @Override
+        public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+            super.onReceivedError(view, request, error);
+            refresh.setVisibility(View.VISIBLE);
+        }
+    }
+}
+
